3a9ec9fcd3b406b63d4b6831b33ecad54d05b5b5
StompingBrokenGlass <stompingbrokenglass@gmail.com>
2013-06-05 15:08:44 +0300

    Added basic properties of the server

--- src/lighting/Server.java
+++ src/lighting/Server.java
@@ -10,5 +10,38 @@ package lighting;
  * @since 2013-06-05
  */
 public class Server {
+    private String hostname = "localhost";
+    private int port = 6667;
     
+    /**
+     * 
+     * @param hostname 
+     */
+    public void setHostname (String hostname){
+        this.hostname = hostname;
+    }
+    
+    /**
+     * 
+     * @return Returns the server's hostname
+     */
+    public String getHostname () {
+        return this.hostname;
+    }
+    
+    /**
+     * 
+     * @param port 
+     */
+    public void setPort (int port){
+        this.port = port;
+    }
+    
+    /**
+     * 
+     * @return the server's port number
+     */
+    public int getPort (){
+        return this.port;
+    }
 }
