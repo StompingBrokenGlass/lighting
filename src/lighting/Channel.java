b2e8e912866d1ffcadd8a82223fa174e0b687afd
StompingBrokenGlass <stompingbrokenglass@gmail.com>
2013-06-05 16:10:12 +0300

    Added basic properties of the user and channel

--- src/lighting/Channel.java
+++ src/lighting/Channel.java
@@ -4,11 +4,43 @@
  */
 package lighting;
 
+import java.util.HashMap;
 /**
  *
  * @author StompingBrokenGlass <stompingbrokenglass@gmail.com>
  * @since 2013-06-05
  */
 public class Channel {
+    private String name;
+    private HashMap users;
     
+    /**
+     * Set the channel name.
+     * @param channelName the channel name including the prefix (eg. #help)
+     */
+    public void setName (String channelName){
+        char beginning = channelName.charAt(0);
+        
+        if (beginning == '&' || beginning == '#' || beginning == '+'
+                || beginning =='!'){
+            this.name = channelName;
+        }
+    }
+    
+    /** 
+     * 
+     * @return the channel name
+     */
+    public String getName () {
+        return name;
+    }
+    
+    public void addUser (User user){
+        this.users.put(user.getName(), user);
+    }
+    
+    public User getUser (String username){
+        User user = (User)this.users.get(username);
+        return user;
+    }
 }
--- src/lighting/User.java
+++ src/lighting/User.java
@@ -10,5 +10,28 @@ package lighting;
  * @since 2013-06-05
  */
 public class User {
+    private String name ;
+    private boolean away;
     
+    User (String name){
+        this.name = name;
+        this.away = false;
+    }
+    
+    public void setName (String name){
+        this.name = name;
+    }
+    
+    public String getName (){
+        return this.name;
+        
+    }
+    
+    public boolean isAway(){
+        return away;
+    }
+    
+    public void toggleAway (){
+        this.away = (! this.away);
+    }
 }
