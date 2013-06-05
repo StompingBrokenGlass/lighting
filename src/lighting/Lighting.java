452f931e994357ee503c9804466cdb682b460f3a
StompingBrokenGlass <stompingbrokenglass@gmail.com>
2013-06-05 14:34:54 +0300

    init

---
+++ src/lighting/Lighting.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lighting;
+
+/**
+ * 
+ * @author StompingBrokenGlass <stompingbrokenglass@gmail.com>
+ * @since 2013-06-05
+ */
+public class Lighting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ src/lighting/Server.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lighting;
+
+/**
+ *
+ * @author StompingBrokenGlass <stompingbrokenglass@gmail.com>
+ * @since 2013-06-05
+ */
+public class Server {
+    
+}
