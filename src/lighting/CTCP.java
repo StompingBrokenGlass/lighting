5aa1cf25ec8f10c224728361f241898167985efe
StompingBrokenGlass <stompingbrokenglass@gmail.com>
2013-06-20 13:42:02 +0300

    Handled Version and send an error for unknown commands

--- src/lighting/CTCP.java
+++ src/lighting/CTCP.java
@@ -11,4 +11,31 @@ package lighting;
  */
 public class CTCP {
     
+    
+    
+    public String processCTCP(String query){
+        
+        String response = "";
+        int space = query.indexOf(" ");
+        
+        String command ;
+        String paramenters ;
+        if (space < 0){
+            command = query;
+        } else {
+            command = query.substring(0,space);
+            paramenters = query.substring(space+1);
+        }
+        
+        switch (command){
+            case "VERSION" :
+                response = "VERSION Lighting-catbot ALPHA";
+                break;
+            default:
+                // send an error message identifying an unknown command
+                response = "ERRMSG " + query + " :Unknown CTCP query";
+        }
+        
+        return response;
+    }
 }
